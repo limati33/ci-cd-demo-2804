d771e3506c91bc0568188a14e503652bc3b3b034
limon3089 <mannaftamirlan1@gmail.com>
2025-04-28 09:38:24 +0500

    UpDaTe

---
+++ index.js
@@ -0,0 +1,6 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Hello, CI/CD!'));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
